9225a20864134c35fd56318cf979f8de93f69bad
Anastasiia Istomina <anastasiia.istomina@post.runi.ac.il>
2025-11-12 22:13:34 +0200

    Completed assignment

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,21 @@
-// Computes an approximation of PI.
+
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int num = Integer.parseInt(args [0]);
+		double pi = Math.PI;
+		int i = 0;
+		double n = 1;
+		int minus = 1;
+		double sum = 0;
+		while(i < num){
+			double term = 1 / n;
+			sum = sum + (term * minus);
+			minus *= (-1);
+			n += 2;
+			i++;
+			
+		}
+		System.out.println("pi according to Java: " + pi);
+		System.out.println("pi, approximated:     " + sum * 4);
 	}
 }
